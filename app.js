4514e812df9cf32667d74115d5db8a35d408370b
RAMESSESII2 <stmbhardwaj@gmail.com>
2020-02-29 11:28:50 +0530

    app initial

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
-const express= require("express");
+const express= require("express"),
+	app= express();
+app.set('view engine', 'ejs');
 
+app.get('/',(_, res)=>{
+	res.render("home");
+
+});
+app.listen(process.env.PORT||3000, process.env.IP, ()=>{
+	console.log("Server started");
+});
