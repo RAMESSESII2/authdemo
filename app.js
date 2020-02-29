b5ccc201d39ef22f530e051719f425e93a1e93c0
RAMESSESII2 <stmbhardwaj@gmail.com>
2020-02-29 11:50:51 +0530

    secrets page

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.get('/',(_, res)=>{
 	res.render("home");
 
 });
+
+app.get("/secret",function(req, res){
+	res.render("secret");
+});
 app.listen(process.env.PORT||3000, process.env.IP, ()=>{
 	console.log("Server started");
 });
