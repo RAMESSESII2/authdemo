2b4390d012a2855fbdc92543e2722be81f37de37
RAMESSESII2 <stmbhardwaj@gmail.com>
2020-03-01 21:28:38 +0530

    setting basic passport

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 const express= require("express"),
-	app= express();
+	app= express(),
+	mongoose=require("mongoose"),
+	passport=require("passport"),
+	bodyParser=require("body-parser"),
+	User=require("./models/user"),
+	LocalStrategy=require("passport-local"),
+	passportLocalMongoose=require("passport-local-mongoose");
+
+	
+mongoose.connect("mongodb://localhost:27017/authdemo",{useUnifiedTopology: true});
 app.set('view engine', 'ejs');
+app.use(require("express-session")({
+	secret:"Ozymandias is the key",
+	resave:false,
+	saveUninitialized:false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get('/',(_, res)=>{
 	res.render("home");
